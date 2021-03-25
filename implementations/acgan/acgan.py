c8a6ecb02045bb2272de7eb44d1fe1c8b51642ac
xieldy <454888912@qq.com>
2021-03-25 22:20:47 +0800

    update

--- implementations/acgan/acgan.py
+++ implementations/acgan/acgan.py
@@ -299,5 +299,5 @@ for epoch in range(opt.n_epochs):
             % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), 100 * d_acc, g_loss.item())
         )
         batches_done = epoch * len(dataloader) + i
-        #if batches_done % opt.sample_interval == 0:
-            #sample_image(n_row=10, batches_done=batches_done)
+        if batches_done % opt.sample_interval == 0:
+            sample_image(n_row=10, batches_done=batches_done)
